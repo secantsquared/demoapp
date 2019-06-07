d629b4d93bc629e7d06fe98407d7ca12e15b709c
ryanboris <ryan.boris@gmail.com>
2019-06-07 02:21:10 -0400

    adds root

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey!</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
